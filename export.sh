a1cdefbfe7296007fc5ac263dc3d9e9fb27cf3c9
Pierrick <mail@pierrickmarie.info>
2022-08-31 15:10:40 +0200

    Export script to publish last updates

---
+++ export.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "update dev branch"
+echo "=============="
+
+hugo
+
+git add -A
+git commit -m "Last update"
+git push 
+
+echo "update main branch"
+echo "=============="
+
+git checkout main
+git checkout dev -- public
+
+mv public/* ./
+rm public
+
+git add -A
+git commit -m "Last update"
+git push 
