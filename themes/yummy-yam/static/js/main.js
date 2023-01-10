62359b0cf72c0c87894487732a237f744040c7eb
Pierrick <mail@pierrickmarie.info>
2023-01-10 16:59:29 +0100

    Rotate avatar on click on navbar button

--- themes/yummy-yam/static/js/main.js
+++ themes/yummy-yam/static/js/main.js
@@ -1,5 +1,8 @@
 var HEADER_HEIGHT = 0;	// The height of the navbar
 
+var ROTATION_ANGLE = "15deg";
+var avatarIsRotated = false;	// status of avatar: rotated or not
+
 $(document).ready(function () {
 
 	HEADER_HEIGHT = $('header').height();
@@ -7,7 +10,43 @@ $(document).ready(function () {
 	$(window).scroll(function () {
 		scrollFunction();
 	});
+
+	setupAvatarRotations();
 });
+
+/**
+ * Setup avatar rotations:
+ * 	- on click on navbar button
+ * 	- on mouse over
+ */
+function setupAvatarRotations() {
+
+	$('.avatar-img').mouseenter(function() {	// mouse entre: rotate avatar
+		$('.avatar-img').css('rotate', `-${ROTATION_ANGLE}`);
+	});
+	
+	$('.avatar-img').mouseout(function() {	// mouse out: reset avatar rotation
+		$('.avatar-img').css('rotate', '0deg');
+	});
+
+	$('#navbar-button').click(function() {	// on click on navbar button
+		rotateAvatar();
+	});
+}
+
+/**
+ * Rotate avatar on expend navbar
+ */
+function rotateAvatar() {
+
+	if(avatarIsRotated) {
+		$('.avatar-img').css('rotate', '0deg');	// reset avatar rotation
+		avatarIsRotated = false;
+	} else {
+		$('.avatar-img').css('rotate', `+${ROTATION_ANGLE}`);	// rotate avatar
+		avatarIsRotated = true;
+	}
+}
 
 /**
  * Fade out navbar after scroll down.
