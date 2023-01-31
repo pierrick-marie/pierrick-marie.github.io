2e0ae83e17d457734056e861269104802a19fb8d
Pierrick <mail@pierrickmarie.info>
2023-01-31 01:21:11 +0100

    Add import script

---
+++ import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm ./*.html
+rm ./*.xml
+
+rm -r categories
+rm -r css
+rm -r img
+rm -r js
+rm -r resources
+rm -r sass
+rm -r tags
+
+git checkout dev.yummy-yam.export -- public
+mv public/* ./
+rmdir public
