4bb560619e29d523d973356302ee2813246dc062
Pierrick <mail@pierrickmarie.info>
2023-02-09 17:18:06 +0100

    Add robots.txt file

--- import.sh
+++ import.sh
@@ -3,18 +3,14 @@
 rm ./*.html
 rm ./*.xml
 
-rm -r categories
 rm -r css
 rm -r img
 rm -r js
 rm -r resources
 rm -r sass
-rm -r tags
 
 git checkout dev.yummy-yam.export -- public
 mv public/* ./
 
 rmdir public
 
-rm -r categories
-rm -r tags
