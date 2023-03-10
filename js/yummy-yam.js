938d37306bd6551cb3c3c98bd3dbfa1b67d8bde9
Pierrick <mail@pierrickmarie.info>
2023-03-10 12:17:16 +0000

    Export last version with Yummy Yam 3.0

--- js/yummy-yam.js
+++ js/yummy-yam.js
@@ -30,10 +30,8 @@ var YummyYam = {
 	 */
 	setupCornerImages: function() {
 
-		var origin = window.location.origin;
-
-		$('.yummy-yam-corner-images').append(`<img class="corner-image bottom-left large" alt="corner image bottom left" src="${origin}/img/theme/corner-left.png">
-			<img class="corner-image bottom-right large" alt="corner image bottom right" src="${origin}/img/theme/corner-right.png">`);
+		$('.yummy-yam-corner-images').append(`<img class="corner-image bottom-left large" alt="corner image bottom left" src="./img/theme/corner-left.png">
+			<img class="corner-image bottom-right large" alt="corner image bottom right" src="./img/theme/corner-right.png">`);
 	},
 
 	/**
